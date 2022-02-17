2b20f955d9e4aae52b749e2db0b6f67fbf4f7bda
Rodrigo <Rodrigo@LAPTOP-CS2C463N>
2022-02-17 01:46:00 -0300

    Plantilla Inicial

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetchData();
+});
+
+const fetchData = async () => {
+    try{
+        const res = await fetch('https://test-api-bsale.herokuapp.com/api/Product');
+        const data = await res.json();        
+        console.log(data);
+    }catch (error){
+        console.log(error);
+    }
+}
